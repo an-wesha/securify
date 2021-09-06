63871823f76d178e5c68271cd9ebb53ec43cffe5
an-wesha <68744661+an-wesha@users.noreply.github.com>
2021-09-06 12:53:20 +0530

    Update Securify_MotionSensor.ino

--- Securify_MotionSensor.ino
+++ Securify_MotionSensor.ino
@@ -1,19 +1,23 @@
-int ledPin = 13;                // LED 
+int ledPin = 13;                // LED (optional)
 int pirPin = 2;                 // PIR Out pin 
 int pirState = 0;                   // PIR status
-const int buzzer = 10;
+const int buzzer = 10;            //buzzer pin
+
 void setup() {
  pinMode(ledPin, OUTPUT);     
  pinMode(pirPin, INPUT);     
- 
 }
+
 void loop(){
  pirState = digitalRead(pirPin); 
- if (pirState == HIGH) {            // if motion detected
-   digitalWrite(ledPin, HIGH);  // turn LED ON
-   tone(buzzer, 440, 2000); //Buzzer goes off
+ if (pirState == HIGH) 
+ {            
+  // if motion detected
+   digitalWrite(ledPin, HIGH);  // turn LED ON indicating movement detected
+   tone(buzzer, 440, 2000);  //440 = frequency, 2000 duration in ms
+  //Buzzer goes off
  } 
  else {
-   digitalWrite(ledPin, LOW); // turn LED OFF if we have no motion
+   digitalWrite(ledPin, LOW); // keep LED OFF if we have no motion
  }
 } 
